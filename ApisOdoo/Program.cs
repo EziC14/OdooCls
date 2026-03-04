fb737580510e3e280108c7894f98b4b57e1aaa4f
EziC14 <esaul.z1407@gmial.com>
2026-03-04 14:12:34 -0500

    Fix validaciones

--- ApisOdoo/Program.cs
+++ ApisOdoo/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
 using OdooCls.API.Attributes;
+using OdooCls.Core.Entities;
 using OdooCls.Core.Interfaces;
 using OdooCls.Infrastucture.Repositorys;
 
@@ -24,6 +26,23 @@ builder.Services.AddTransient<IRegistroMovimientosRepository, RegistroMovimiento
 builder.Services.AddControllers(options =>
 {
     options.Filters.Add<BearerAuthAttribute>(); // Aplica a toda la API
+});
+
+builder.Services.Configure<ApiBehaviorOptions>(options =>
+{
+    options.InvalidModelStateResponseFactory = context =>
+    {
+        var errores = context.ModelState
+            .Where(e => e.Value?.Errors.Count > 0)
+            .Select(e => $"{e.Key}: {e.Value!.Errors.First().ErrorMessage}");
+
+        var mensaje = errores.Any()
+            ? $"Datos inválidos. {string.Join(" | ", errores)}"
+            : "Datos inválidos en la solicitud";
+
+        var response = new ApiResponse<object>(400, 1009, mensaje, null);
+        return new BadRequestObjectResult(response);
+    };
 });
 
 
